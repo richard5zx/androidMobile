31cabe42a6d95b8591451e38888e7057be2c3863
richard5zx <richardchao2009@gmail.com>
2025-02-03 20:57:51 +0800

    initialize variables

--- project01_calc/app/src/main/java/com/example/p01_calc/MainActivity.java
+++ project01_calc/app/src/main/java/com/example/p01_calc/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.p01_calc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    private EditText etA, etB;
+    private TextView tvOper, tvResult;
+    private Button[] btns = new Button[6];
+    private int[] btnIds = {R.id.btnAdd, R.id.btnSub, R.id.btnMul, R.id.btnDiv, R.id.btnCal, R.id.btnReset};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,22 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        initView();
 
     }
+
+    private void initView() {
+        etA = findViewById(R.id.etA);
+        etB = findViewById(R.id.etB);
+        tvOper = findViewById(R.id.tvOper);
+        tvResult = findViewById(R.id.tvResult);
+        for (int i = 0; i < btns.length; i++) {
+            btns[i] = findViewById(btnIds[i]);
+        }
+    }
+
+    public void onClick(View view) {
+
+    }
+
 }
